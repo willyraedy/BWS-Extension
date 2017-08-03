4c7cddda80acbf0738f41fd5dd73d40898d1bebc
Willy Raedy <wraedy@gmail.com>
2017-08-03 11:27:52 -0500

    finish file restructure

--- ContentScripts/cs.js
+++ ContentScripts/cs.js
@@ -26,8 +26,6 @@ chrome.storage.local.get('whitelist', function (whitelistedObj) {
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-
-
         <style>
 
           .f-box {
