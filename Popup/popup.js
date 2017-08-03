111e77919f603d72e12337906d75ee1be4cf0297
Willy Raedy <wraedy@gmail.com>
2017-08-03 11:38:08 -0500

    hide buttons if company highly rated

--- Popup/popup.js
+++ Popup/popup.js
@@ -74,12 +74,15 @@ chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs)
 
         case 'A':
           $('#grade-explanation').text('Companies in the \'A\' range are leaders in the areas of social and environmental responsibiity. Their efforts lay the groundworks for the future of sustainable business worldwide. Often these companies were created specifically to provide socially and environmentally responsible options for consumrs. While none of these companies are perfect, these are about as close as we can get at this stage in our evolving marketplace. Consider going out of your way to support these companies as their efforts are essential to bringing along all of the rest of the companies.')
+          $('#bad-grade-options').hide();
           break;
         case 'B':
           $('#grade-explanation').text('Companies that fall into the \'B\' range tend to represent mainstream companies that are making significant progress in turing toward people/planet friendly behaviors. Most large companies peak in this range if they are making a concerted effort to be more responsible. While these compnaies still have a long way to go, their effort should be recognized as they provide a solid alternative to many of their competitors in the same industry.')
+          $('#bad-grade-options').hide();
           break;
         case 'C':
           $('#grade-explanation').text('Companies that fall into the \'C\' range either have mixed responsibility records or little or no data exist to rank them relative to the other companies. If you have no data about a particular brand or company, you should almost always assume they fall into this category. Until companies are required to release verifiable data on their social and environmentally relevant behavior, most will end up as C companies. While they are not typically companies worth going out of your way to support, these companies provide us with options when the alternatives are D and F ranked businesses.')
+          $('#bad-grade-options').hide();
           break;
         case 'D':
           $('#grade-explanation').text('If a company ends up in the \'D\' range, it is likely engaged in practices that have measurably negative consequences for humans and the environment. Typically these company\'s unsustainable behavior is being tracked by a number of independent, third-parties around more than one issue. It is important to hold these companies accountable for their behavior and to clearly communicat that their practices need to be changed if they are going to earn back the trust of concerned citizens and ethical consumers. You should consider avoiding these companies and brands when possible in order to send this economic message in a way that will be heard.')
@@ -95,7 +98,7 @@ chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs)
 
     } else if (paused) {
       $('#grade-statement').text('Better World Shopping Guide is paused')
-      $('#options').hide();
+      $('#bad-grade-options').hide();
       $('#pause-btn').text('Un-pause Better World Shopping Guide')
       $('#pause-btn').on('click', function () {
         chrome.storage.local.set({ paused: false }, function () {
@@ -106,7 +109,7 @@ chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs)
         })
       })
     } else {
-      $('#options').hide();
+      $('#bad-grade-options').hide();
     }
   });
 
